346bd6929686270f68be0f1a29fc0bfc51b5cfc7
King Hua <kinghua0629@outlook.com>
2026-06-25 23:03:27 +0800

    modify dependencies
    Signed-off-by: King Hua <kinghua0629@outlook.com>

--- src/config/expressiveCodeConfig.ts
+++ src/config/expressiveCodeConfig.ts
@@ -8,10 +8,10 @@ import type { ExpressiveCodeConfig } from "../types/expressiveCodeConfig";
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// 暗色主题（用于暗色模式）
-	darkTheme: "one-dark-pro",
+	darkTheme: "github-dark",
 
 	// 亮色主题（用于亮色模式）
-	lightTheme: "one-light",
+	lightTheme: "github-light",
 
 	// 更多样式请看expressive-code的官方文档
 	// https://expressive-code.com/guides/themes/
@@ -27,6 +27,6 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// 语言徽章插件配置
 	pluginLanguageBadge: {
 		// 是否启用语言徽章插件
-		enable: false,
+		enable: true,
 	},
 };
